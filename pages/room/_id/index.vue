e3dc54cd3cc174f9a875e8d810020c1f39e37d11
Alf Sebastian Houge <git@alfhouge.no>
2020-08-27 18:44:17 +0200

    Style lobby page a little bit

--- pages/room/_id/index.vue
+++ pages/room/_id/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
-    <h1 class="title is-2">Velkommen {{name}}</h1>
-    <h1 class="title is-3">Venter på deltakere</h1>
-    {{roomId}}
+  <div class="main-content">
+    <h1 class="title is-2">
+      Velkommen
+      <span id="name">{{name}}</span>
+    </h1>
+    <p>Kopier denne koden og gi til vennene dine</p>
+    <b-field id="roomInput">
+      <b-input readonly="true" :value="roomId" v-model="roomId"></b-input>
+    </b-field>
+
+    <b-button
+      @click="toggleReady"
+      size="is-large"
+      :icon-left="readyButtonIcon"
+      :type="readyButtonColor"
+      inverted
+    >{{readyButtonText}}</b-button>
   </div>
 </template>
 
@@ -13,7 +26,39 @@ export default {
     return {
       roomId: this.$route.params.id,
       name: this.$route.query.name,
+      ready: false,
     };
+  },
+  computed: {
+    readyButtonIcon: function () {
+      return this.ready ? "cancel" : "check";
+    },
+    readyButtonText: function () {
+      return this.ready ? "Avbryt" : "Klar?";
+    },
+    readyButtonColor: function () {
+      return this.ready ? "is-danger" : "is-primary";
+    },
+  },
+  methods: {
+    toggleReady: function () {
+      this.ready = !this.ready;
+    },
   },
 };
 </script>
+<style lang="scss" scoped>
+.main-content {
+  margin: 2em auto;
+  width: 50%;
+}
+#roomInput {
+  margin-top: 1em;
+  width: 5.5em;
+}
+#name {
+  text-decoration: underline;
+  text-decoration-skip-ink: auto;
+  color: white;
+}
+</style>
